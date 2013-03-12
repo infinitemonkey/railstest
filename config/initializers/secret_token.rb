c8c3fb441eeb44fc0328322eef61fa6f885749b8
R23b <r23b@R23bs-MacBook.local>
2013-03-12 10:29:31 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstest::Application.config.secret_token = '9104e743ec05cb471edc502b0e2db8fa1c3add269e4e1f577180d30ef42e78bd19d17ef2a217a07404f98caec07b44b907f18be5ad4f646310b40f29e21a7478'
