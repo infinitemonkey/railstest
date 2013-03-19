a07ce9b6b9bb7c37a837c17bada3ae814d7b3fc9
Stefan Sidler <ste.sidler@gmail.com>
2013-03-19 15:38:20 +0100

    inital product tests

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,17 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "new product is invalid" do
+    product = Product.new
+
+    assert product.invalid?
+  end
+
+  test "dont save invalid product" do
+    product = Product.new
+
+    assert !product.save
+  end
+
 end
