da06679fb2b76c1720324c047b58b8fef7ba1967
Stefan Sidler <stefansidler@Stefans-MacBook-Pro.local>
2013-04-09 15:09:43 +0200

    added basic cart func

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,18 @@ class Product < ActiveRecord::Base
 
   validates_presence_of :title, :description, :price
   validates_numericality_of :price
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+# ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
