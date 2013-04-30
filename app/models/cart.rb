d0d73d2ea1da37f603684a0c4b7a1252150b9e28
Stefan Sidler <stefansidler@Stefans-MacBook-Pro.local>
2013-04-30 15:36:57 +0200

    log info

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,6 +5,7 @@ class Cart < ActiveRecord::Base
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
   if current_item
+    logger.info current_item.inspect
     current_item.quantity += 1
   else
     current_item = line_items.build(product_id: product_id)
